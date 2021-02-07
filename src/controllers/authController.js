717145cf513ca289f45e58978d4d1d577780795c
leandrocodes <leandrovianacodes@gmail.com>
2021-02-07 15:38:34 -0300

    add user login

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -8,10 +8,16 @@ const userRegister = async (req, res) => {
     bcrypt.hash(req.body.password, 8, async (err, hashedPass) => {
       if (err) {
         console.log(err)
-        return res.status(400).send({ error: 'register failed' })
+        return res.status(400).send({
+          error: 'register failed'
+        })
       }
       const { name, email } = req.body
-      const user = await User.create({ name, email, password: hashedPass })
+      const user = await User.create({
+        name,
+        email,
+        password: hashedPass
+      })
       return res.send({ user })
     })
   } catch (err) {
@@ -20,4 +26,37 @@ const userRegister = async (req, res) => {
   }
 }
 
-module.exports = { userRegister }
+const userLogin = async (req, res) => {
+  const { email, password } = req.body
+
+  try {
+    const user = await User.findOne({ email })
+    if (user) {
+      bcrypt.compare(password, user.password, (err, result) => {
+        if (err) {
+          console.error(err)
+          res.status(400).json({
+            error: `Password don't match`
+          })
+        }
+
+        if (result) {
+          const token = jwt.sign({ email, password }, 'baguvix')
+          res.status(200).json({ token })
+        }
+      })
+    } else {
+      throw new Error('fail')
+    }
+  } catch (err) {
+    console.log(err)
+    return res.status(400).send({
+      error: 'login failed'
+    })
+  }
+}
+
+module.exports = {
+  userRegister,
+  userLogin
+}
