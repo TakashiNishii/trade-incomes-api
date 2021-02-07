57497f4a3f4b2f80b38ab84b585f919d11ffc652
leandrocodes <leandrovianacodes@gmail.com>
2021-02-07 15:37:56 -0300

    add login route

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -4,5 +4,6 @@ const authController = require('../controllers/authController')
 const router = Router()
 
 router.post('/register', authController.userRegister)
+router.post('/login', authController.userLogin)
 
 module.exports = router
