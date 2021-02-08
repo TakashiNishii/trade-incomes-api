9c5ea4d292cde3752779bc7d22bdcb679866db45
leandrocodes <leandrovianacodes@gmail.com>
2021-02-08 16:50:53 -0300

    admin verification middleware

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -1,9 +1,29 @@
 const jwt = require('jsonwebtoken')
 
+const User = require('../models/user')
+
 const verifyAdmin = (req, res, next) => {
-  const token = req.headers.authorization.split(' ')[1]
-  console.info(token)
-  next()
+  const bearer = req.headers.authorization
+  if (bearer) {
+    const token = bearer.split(' ')[1]
+    jwt.verify(token, 'baguvix', async (err, user) => {
+      if (err) return res.status(403).send({ error: 'invalid jwt' })
+
+      if (user) {
+        console.log(user)
+
+        const isAdmin = await User.findOne({
+          email: user.email,
+          admin: true
+        }).lean()
+
+        if (!isAdmin) return res.status(403).send({ error: 'not authorized' })
+        else next()
+      }
+    })
+  } else {
+    return res.status(403).send({ error: 'not authorized' })
+  }
 }
 
 module.exports = { verifyAdmin }
