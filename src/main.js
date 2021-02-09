29984329d93e497d74e9c005645e8c76b8686602
leandrocodes <leandrovianacodes@gmail.com>
2021-02-09 09:47:32 -0300

    dot env install

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 
+const dotenv = require('dotenv')
+dotenv.config()
+
 const app = express()
 
 app.use(bodyParser.json())
@@ -9,4 +12,9 @@ app.use(bodyParser.urlencoded({ extended: false }))
 const authRoutes = require('./routes/auth')
 app.use('/auth/', authRoutes)
 
-app.listen(3000)
+const server = app.listen(3000, () => {
+  const host = server.address().address
+  const port = server.address().port
+
+  console.log(`Server listening at http://${host}:${port}`)
+})
