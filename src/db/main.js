e9cb15c52361415cee658e3b58f6f1f260b218ed
leandrocodes <leandrovianacodes@gmail.com>
2021-02-10 17:30:44 -0300

    fix depreciated functions

--- src/db/main.js
+++ src/db/main.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose')
 
 mongoose.connect('mongodb://localhost/trade-incomes', {
   useNewUrlParser: true,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
+  useCreateIndex: true
 })
 
 mongoose.Promise = global.Promise
