37987e269d68fa599d7585a4643d8158f7a3ba4c
leandrocodes <leandrovianacodes@gmail.com>
2021-02-11 23:20:14 -0300

    localhost db

--- src/db/main.js
+++ src/db/main.js
@@ -1,13 +1,10 @@
 const mongoose = require('mongoose')
 
-mongoose.connect(
-  'mongodb+srv://admin:admin@trade-incomes.va36r.mongodb.net/tradeincomes?retryWrites=true&w=majority',
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true
-  }
-)
+mongoose.connect('mongodb://localhost:27017/trade-incomes', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true
+})
 
 mongoose.Promise = global.Promise
 
