a03387971a2ed541881694d333f4fa6ab09e0a3d
leandrocodes <leandrovianacodes@gmail.com>
2021-02-11 21:03:29 -0300

    mongoose

--- src/db/main.js
+++ src/db/main.js
@@ -1,10 +1,21 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost/trade-incomes', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true
-})
+if (process.env.NODE_ENV === 'development') {
+  mongoose.connect('mongodb://localhost/trade-incomes', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+  })
+} else {
+  mongoose.connect(
+    'mongodb://pixel:thrasher123@geonosis.mongodb.umbler.com:41435/trade-incomes',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true
+    }
+  )
+}
 
 mongoose.Promise = global.Promise
 
