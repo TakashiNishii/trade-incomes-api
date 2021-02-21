61cacc458e055c06d2864bc7e6434222e8a80bfe
leandrocodes <leandrovianacodes@gmail.com>
2021-02-21 18:26:45 -0400

    User update password

--- src/controllers/user/UserActionsController.js
+++ src/controllers/user/UserActionsController.js
@@ -1,6 +1,8 @@
 const User = require('../../models/user')
 const jwt = require('jsonwebtoken')
 
+const bcrypt = require('bcrypt')
+
 const show = async (req, res) => {
   const token = req.headers.authorization.split(' ')[1]
 
@@ -18,7 +20,39 @@ const show = async (req, res) => {
     return res.status(400).json({ error })
   }
 }
+const changePassword = async (req, res) => {
+  const { email, newPassword, oldPassword } = req.body
+
+  try {
+    const user = await User.findOne({ email }).select('+password')
+
+    if (!user) {
+      return res.status(404).json({ error: 'Got error at user load' })
+    }
+
+    const match = await bcrypt.compare(oldPassword, user.password)
+
+    if (!match) {
+      return res.status(406).json({ error: `Password don't match` })
+    }
+
+    const hashedPass = await bcrypt.hash(newPassword, 8)
+
+    const update = { password: hashedPass }
+
+    const result = await User.updateOne({ email }, update)
+
+    if (result) {
+      return res.json({ message: 'Password updated' })
+    }
+  } catch (error) {
+    return res
+      .status(400)
+      .json({ error: 'Cannot update the user password' })
+  }
+}
 
 module.exports = {
-  show
+  show,
+  changePassword
 }
--- src/routes/user.js
+++ src/routes/user.js
@@ -5,5 +5,10 @@ const authMiddleware = require('../middlewares/authMiddleware')
 const router = Router()
 
 router.get('/profile', authMiddleware.verifyUser, userController.show)
+router.put(
+  '/change-password',
+  authMiddleware.verifyUser,
+  userController.changePassword
+)
 
 module.exports = router
