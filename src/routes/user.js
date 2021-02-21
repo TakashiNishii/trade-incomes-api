906ad5f361a75e38fc9369a9f611f5d19e6356de
leandrocodes <leandrovianacodes@gmail.com>
2021-02-21 17:31:34 -0400

    user controllers better renamed

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,9 +1,9 @@
 const { Router } = require('express')
-const userController = require('../controllers/user/userController')
+const userController = require('../controllers/user/UserActions')
 const authMiddleware = require('../middlewares/authMiddleware')
 
 const router = Router()
 
-router.get('/incomes', authMiddleware.verifyUser, userController.show)
+router.get('/profile', authMiddleware.verifyUser, userController.show)
 
 module.exports = router
